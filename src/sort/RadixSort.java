b6dda94d3b6e7f3143e79f178d37c1da44e0bbcc
master <yangli@hanpi.com>
2021-10-30 19:06:33 +0800

    suanfa01

---
+++ src/sort/RadixSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+//基数排序
+
+public class RadixSort {
+    public static void main(String[] args) {
+        int arr[] = {53, 3, 542, 748, 14, 214};
+
+    }
+
+    //基数排序的方法
+    public static void radixSort(int arr[]) {
+        //第一轮排序，针对每个元素的个位排序
+
+        //定义一个二维数组，表示10个桶,每个桶就是一维数组
+        
+
+
+
+    }
+
+}
